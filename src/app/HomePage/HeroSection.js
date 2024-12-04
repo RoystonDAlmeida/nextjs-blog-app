6e75def2eb8a23930ea3e0521dbe6323b823228c
RoystonDAlmeida <roystonjan1@hotmail.com>
2024-12-05 02:33:36 +0530

    Added link for viewing all posts

--- src/app/HomePage/HeroSection.js
+++ src/app/HomePage/HeroSection.js
@@ -1,4 +1,6 @@
 // components/HeroSection.js
+import Link from 'next/link';
+
 export default function HeroSection() {
     return (
     <div className="bg-black text-white p-6 rounded-lg h-1/2 mx-auto mt-2 border-4 border-teal-300 shadow-lg shadow-teal-200/50 transition duration-300" style={{ marginTop: '10px',  marginLeft:'10px', marginRight:'10px' }}>
@@ -10,9 +12,11 @@ export default function HeroSection() {
           <p className="text-lg md:text-xl mb-6">
             Explore insights, stories, and inspiration from my journey. ✍️✨
           </p>
-          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded transition duration-300">
-            View All Posts
-            </button>
+          <Link href='/blog/allPosts'>
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded transition duration-300">
+              View All Posts
+          </button>
+          </Link>
         </div>
       </div>
     </div>
