782376d36d508b159107852257ef8ca0fdd08d1a
RoystonDAlmeida <roystonjan1@hotmail.com>
2024-12-05 02:52:54 +0530

    Implementation of contact form component

---
+++ src/app/contact/page.js
@@ -0,0 +1,10 @@
+// app/contact/page.js
+import Contact from "../ContactPage/Contact";
+
+export default function ContactPage() {
+    return (
+        <>
+            <Contact />
+        </>
+    );
+}
