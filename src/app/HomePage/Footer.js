5711ae9b29d511911a9b522ca7e3a89b67acb937
RoystonDAlmeida <roystonjan1@hotmail.com>
2024-12-05 02:28:55 +0530

    Updated footer for web pages

--- src/app/HomePage/Footer.js
+++ src/app/HomePage/Footer.js
@@ -1,18 +1,20 @@
 // components/Footer.js
 import { FaGithub, FaLinkedin } from 'react-icons/fa';
+import dotenv from 'dotenv';
+dotenv.config();
 
 export default function Footer() {
   return (
     <footer className="bg-gray-800 text-white py-6 mt-4 border-t border-white">
       <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
         <div className="flex-grow text-center mb-4 md:mb-0">
-          <p className="text-sm font-sans">© {new Date().getFullYear()} My Blog. All rights reserved.</p>
+          <p className="text-sm font-sans">© {new Date().getFullYear()} BlogPostView. All rights reserved.</p>
         </div>
         <div className="flex space-x-6">
-          <a href="#" aria-label="GitHub" className="hover:text-blue-400">
+          <a href={process.env.NEXT_PUBLIC_GITHUB_PROFILE_URL} aria-label="GitHub" className="hover:text-blue-400" rel="noopener noreferrer" target="_blank">
             <FaGithub />
             </a>
-          <a href="#" aria-label="LinkedIn" className="hover:text-blue-400">
+          <a href={process.env.NEXT_PUBLIC_LINKEDIN_PROFILE_URL} aria-label="LinkedIn" className="hover:text-blue-400" rel="noopener noreferrer" target="_blank">
             <FaLinkedin />
           </a>
         </div>
